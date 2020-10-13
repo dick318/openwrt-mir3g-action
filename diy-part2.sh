62abb7dcfe22c3c7a7c5664289bf8971d45c28ad
dick318 <liukai94318@sina.com>
2020-10-13 17:59:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+rm -rf package/ctcgfw/luci-theme-atmaterial
+svn co  https://github.com/liuran001/openwrt-packages/trunk/luci-theme-atmaterial package/ctcgfw/luci-theme-atmaterial
